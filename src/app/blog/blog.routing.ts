73153c0425dc1a49cee923ffe03748d4fb4bf1db
Agne <agnepetrylaite@gmail.com>
2017-09-20 15:27:43 +0300

    blog routes added

--- src/app/blog/blog.routing.ts
+++ src/app/blog/blog.routing.ts
@@ -1,11 +1,10 @@
 import {RouterModule, Routes} from "@angular/router";
 import {BlogComponent} from "./blog.component";
-import {AuthGuardService} from "../admin/auth-guard.service";
+import {BlogPostComponent} from "./blog-post/blog-post.component";
 
 const blogRoutes: Routes = [
-    {path: 'blog', component: BlogComponent, pathMatch: 'full', canActivate: [AuthGuardService]},
-    {path: 'blog/all-posts', component: BlogComponent, canActivate: [AuthGuardService]},
-    {path: 'blog/post/:id', component: BlogComponent, canActivate: [AuthGuardService]},
+    {path: 'blog', component: BlogComponent, pathMatch: 'full'},
+    {path: 'blog/:id', component: BlogPostComponent},
 ];
 
 export const blogRouting = RouterModule.forChild(blogRoutes);
