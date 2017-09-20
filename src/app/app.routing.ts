abd815e9b19c7f48d9efec2a833f81a1defc6c66
Agne <agnepetrylaite@gmail.com>
2017-09-20 15:30:53 +0300

    blog components added to routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,15 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {BlogComponent} from "./blog/blog.component";
+import {AboutComponent} from "./about/about.component";
 
 
 const APP_Routes:Routes = [
     {path: '', pathMatch: 'full', component: HomeComponent},
     {path: 'not-found', component: NotFoundComponent},
+    {path: 'blog/all-posts', component: BlogComponent},
+    {path: 'about', component: AboutComponent},
 
 ];
 
